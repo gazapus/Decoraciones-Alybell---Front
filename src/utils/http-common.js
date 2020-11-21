e2a7c3f6a6158863c0232fc16e5af8c28aa5af33
Cristian Villafane <cristianv129@hotmail.com>
2020-11-21 02:22:32 -0300

    Agregada politica CORS al header

--- src/utils/http-common.js
+++ src/utils/http-common.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
     baseURL: process.env.REACT_APP_SERVER || "http://localhost:8080/api/v1",
     headers: {
-        "Content-type": "application/json"
+        "Content-type": "application/json",
+        "Access-Control-Allow-Origin": "*"
     }
 });
