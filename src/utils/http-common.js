08d4a8f9cd663bc669a80098fb79fa81719c3030
Cristian Villafane <cristianv129@hotmail.com>
2020-11-21 02:40:27 -0300

    Otro intento con cors

--- src/utils/http-common.js
+++ src/utils/http-common.js
@@ -4,6 +4,8 @@ export default axios.create({
     baseURL: process.env.REACT_APP_SERVER || "http://localhost:8080/api/v1",
     headers: {
         "Content-type": "application/json",
-        "Access-Control-Allow-Origin": "*"
+        'Access-Control-Allow-Origin' : '*',
+        'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+        'Access-Control-Allow-Credentials': true
     }
 });
