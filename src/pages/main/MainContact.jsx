6a1ae070a3affb27cd0972b1e4c8d51751fbef6e
Cristian Villafane <cristianv129@hotmail.com>
2020-11-23 03:22:14 -0300

    Ocultado mapa hasta que cargue la pagina

--- src/pages/main/MainContact.jsx
+++ src/pages/main/MainContact.jsx
@@ -1,6 +1,8 @@
 import FullScreenPage from '../../components/FullScreenPage';
 import '../../styles/ContactScreen.css';
 import ContactForm from "../../components/ContactForm";
+import { useEffect, useState } from 'react';
+
 /** 
  * Represents contact form component
  * @constructor
@@ -20,6 +22,26 @@ function MainContact({
     buttonText = "#fff",
     footerText = "#fff"
 }) {
+    const [map, setMap] = useState(<p>Cargando</p>)
+
+    useEffect(() => {
+        window.addEventListener('load', () => {
+            setMap( 
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.4095521018694!2d-58.446289784592054!3d-34.593803864480854!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb5f5b97c4b45%3A0x5936bf3b742b1b58!2sVillarroel%201150%2C%20C1414%20AMD%2C%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1604987755236!5m2!1ses-419!2sar"
+                width="100%"
+                height="100%"
+                frameBorder="0"
+                style={{ border: 0 }}
+                allowFullScreen={true}
+                aria-hidden="false"
+                tabIndex="0"
+                title="map">
+            </iframe>)
+        })
+    }, [])
+
+
     return (
         <FullScreenPage
             keyboardOpen={keyboardOpen}
@@ -47,17 +69,7 @@ function MainContact({
                         </div>
                     </div>
                     <div className="Map" style={{ borderColor: borderColor }}>
-                        <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.4095521018694!2d-58.446289784592054!3d-34.593803864480854!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb5f5b97c4b45%3A0x5936bf3b742b1b58!2sVillarroel%201150%2C%20C1414%20AMD%2C%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1604987755236!5m2!1ses-419!2sar"
-                            width="100%"
-                            height="100%"
-                            frameBorder="0"
-                            style={{ border: 0 }}
-                            allowFullScreen={true}
-                            aria-hidden="false"
-                            tabIndex="0"
-                            title="map">
-                        </iframe>
+                        {map}
                     </div>
                 </div>
             </div>
