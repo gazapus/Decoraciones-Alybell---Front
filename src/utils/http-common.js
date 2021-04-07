49cb393b02f7b2c750c7d7fb9c13aa24b61e0679
Cristian Villafane <cristianv129@hotmail.com>
2021-04-07 16:18:11 -0300

    firebase added

--- src/utils/http-common.js
+++ src/utils/http-common.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const hostURL = process.env.REACT_APP_SERVER || "https://decoracionesaly.herokuapp.com/api/v1" ||"http://localhost:8080/api/v1";
+
 export default axios.create({
-    baseURL: process.env.REACT_APP_SERVER || "http://localhost:8080/api/v1",
+    baseURL: hostURL,
     headers: {
         "Content-type": "application/json",
         'Access-Control-Allow-Origin' : '*'
